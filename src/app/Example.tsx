23afb01742920060c4fff96f324de2281775eec0
sshen070 <SujithShen@gmail.com>
2025-10-08 21:02:47 -0700

    Modified objects to resemble 2x rectangles & 2x pill shaped objects

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,19 +1,33 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+import React from "react";
+
+const Example = (props: {
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}) => {
   return (
-    <div className="bg-pickleball-green-200 flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-pickleball-lime-100 w-1/2 p-5 text-left">
+    // Outer rectangle
+    <h1 className="bg-pickleball-green-200 flex w-2/3 flex-col items-center justify-center gap-6 rounded-2xl p-10">
+      {/* Top rectangle */}
+      <h2 className="bg-pickleball-green-100 px-30 py-4 text-center text-xl text-white">
         {props.text1}
-      </div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-pickleball-lime-100 w-full p-5 text-left">
-          props.text2
-        </div>
-        <div className="bg-pickleball-lime-100 w-full p-5 text-right">
+      </h2>
+
+      {/* Inner Pill Shaped */}
+      <h2 className="flex gap-1">
+        <h3 className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-right">
           {props.text2}
-        </div>
-      </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
-    </div>
+        </h3>
+        <h3 className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-left">
+          {props.text3}
+        </h3>
+      </h2>
+
+      <h2 className="bg-pickleball-blue-100 px-30 py-4 text-center text-xl text-black">
+        {props.text4}
+      </h2>
+    </h1>
   );
 };
 
