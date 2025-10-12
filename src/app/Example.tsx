4008351d8c979ca699339f0355424b65eb9d73ca
sshen070 <SujithShen@gmail.com>
2025-10-12 15:55:17 -0700

    Destructured props & made other modifications

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,34 +1,34 @@
 import React from "react";
 
-const Example = (props: {
+interface ComponentProps {
   text1: string;
   text2: string;
   text3: string;
   text4: string;
-}) => {
+
+}
+
+const ComponentProps = ({text1, text2, text3}: ComponentProps) =>{
   return (
-    // Outer rectangle
-    <h1 className="bg-pickleball-green-200 flex w-2/3 flex-col items-center justify-center gap-6 rounded-2xl p-10">
-      {/* Top rectangle */}
-      <h2 className="bg-pickleball-green-100 px-30 py-4 text-center text-xl text-white">
-        {props.text1}
-      </h2>
+    <div className="bg-pickleball-green-200 flex w-2/3 flex-col items-center justify-center gap-6 rounded-2xl p-10">
+      <div className="bg-pickleball-green-100 px-30 py-4 text-center text-xl text-white rounded-md">
+        {text1}
+      </div>
 
-      {/* Inner Pill Shaped */}
-      <h2 className="flex gap-1">
-        <h3 className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-right">
-          {props.text2}
-        </h3>
-        <h3 className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-left">
-          {props.text3}
-        </h3>
-      </h2>
+      <div className="flex gap-1">
+        <div className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-right">
+          {text2}
+        </div>
+        <div className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-left">
+          {text2}
+        </div>
+      </div>
 
-      <h2 className="bg-pickleball-blue-100 px-30 py-4 text-center text-xl text-black">
-        {props.text4}
-      </h2>
-    </h1>
+      <div className="bg-pickleball-blue-100 px-30 py-4 text-center text-xl text-black">
+        {text3}
+      </div>
+    </div>
   );
 };
 
-export default Example;
+export default ComponentProps;
