5278617eddb6da04442e3de48dc4b3a7ab24bc07
ManuChint <manu.chintalapathi@gmail.com>
2025-10-15 14:41:08 -0700

    destructurized the props

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -1,19 +1,25 @@
-const Example = (props: { text1: string; text2: string; text3: string }) => {
+interface ExampleProps {
+  text1: string;
+  text2: string;
+  text3: string;
+}
+
+const Example = ({ text1, text2, text3 }: ExampleProps) => {
   return (
     <div className="bg-pickleball-green-200 flex w-2/3 flex-col items-center gap-9 justify-self-center rounded-2xl p-8">
       <div className="bg-pickleball-green-100 w-1/2 p-5 text-center text-2xl text-white">
-        {props.text1}
+        {text1}
       </div>
       <div className="flex w-1/2 flex-row gap-1">
         <div className="bg-pickleball-lime-100 w-full rounded-full p-5 text-center text-2xl">
-          {props.text2}
+          {text2}
         </div>
         <div className="bg-pickleball-lime-100 w-full rounded-full p-5 text-center text-2xl">
-          {props.text2}
+          {text2}
         </div>
       </div>
       <div className="bg-pickleball-blue-100 w-1/2 p-5 text-center text-2xl">
-        {props.text3}
+        {text3}
       </div>
     </div>
   );
