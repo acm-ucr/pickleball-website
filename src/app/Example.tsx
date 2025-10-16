464b839c690b56496a43f7a2451fc1d76805529f
sshen070 <SujithShen@gmail.com>
2025-10-15 21:24:25 -0700

    Adjusted width of the divs to match image & justify-self-center

--- src/app/Example.tsx
+++ src/app/Example.tsx
@@ -8,21 +8,21 @@ interface ComponentProps {
 
 const ComponentProps = ({ text1, text2, text3 }: ComponentProps) => {
   return (
-    <div className="bg-pickleball-green-200 flex w-2/3 flex-col items-center justify-center gap-6 rounded-2xl p-10">
-      <div className="bg-pickleball-green-100 rounded-md px-30 py-4 text-center text-xl text-white">
+    <div className="bg-pickleball-green-200 flex w-2/3 flex-col items-center justify-center gap-8 rounded-2xl p-10 justify-self-center">
+      <div className="bg-pickleball-green-100 w-1/2 py-4 text-center text-xl text-white">
         {text1}
       </div>
 
-      <div className="flex gap-1">
-        <div className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-right">
+      <div className="flex w-1/2 gap-1" >
+        <div className="bg-pickleball-lime-100 flex-1 flex items-center justify-center rounded-full py-4 text-lg text-black">
           {text2}
         </div>
-        <div className="bg-pickleball-lime-100 w-full rounded-4xl p-5 px-12 text-left">
+        <div className="bg-pickleball-lime-100 flex-1 flex items-center justify-center rounded-full py-4 text-lg text-black">
           {text2}
         </div>
       </div>
 
-      <div className="bg-pickleball-blue-100 px-30 py-4 text-center text-xl text-black">
+      <div className="bg-pickleball-blue-100 w-1/2 py-4 text-center text-xl text-black">
         {text3}
       </div>
     </div>
