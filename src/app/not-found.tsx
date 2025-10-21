e10ae04151498a224631c2419c6e731c48707182
ManuChint <manu.chintalapathi@gmail.com>
2025-10-21 12:51:45 -0700

    changed issues with style and button behavior

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,16 @@
-"use client";
+import Link from "next/link";
 
 const NotFound = () => {
-  const goHome = () => {
-    window.location.href = "/";
-  };
   return (
-    <div className="bg-pickleball-green-100 flex h-screen w-screen flex-col items-center justify-center">
-      <p className="text-pickleball-lime-100 text-9xl font-medium">404</p>
-      <p className="mb-5 text-5xl font-medium text-white">Page Not Found</p>
-      <button
-        className="bg-pickleball-lime-100 text-pickleball-green-200 rounded-full px-12 py-4 text-3xl font-thin"
-        onClick={goHome}
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <p className="text-pickleball-lime-100 text-9xl">404</p>
+      <p className="mb-5 text-5xl text-white">Page Not Found</p>
+      <Link
+        href="/"
+        className="bg-pickleball-lime-100 text-pickleball-green-200 rounded-full px-12 py-4 text-3xl transition-opacity hover:opacity-70"
       >
         Back to Home
-      </button>
+      </Link>
     </div>
   );
 };
