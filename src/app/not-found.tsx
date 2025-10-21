b0f7e05bb254c2acdd79b6464d151763945e978b
ManuChint <manu.chintalapathi@gmail.com>
2025-10-20 18:27:24 -0700

    completed not found page with button component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,21 @@
+"use client";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  const goHome = () => {
+    window.location.href = "/";
+  };
+  return (
+    <div className="bg-pickleball-green-100 flex h-screen w-screen flex-col items-center justify-center">
+      <p className="text-pickleball-lime-100 text-9xl font-medium">404</p>
+      <p className="mb-5 text-5xl font-medium text-white">Page Not Found</p>
+      <button
+        className="bg-pickleball-lime-100 text-pickleball-green-200 rounded-full px-12 py-4 text-3xl font-thin"
+        onClick={goHome}
+      >
+        Back to Home
+      </button>
+    </div>
+  );
 };
 
 export default NotFound;
