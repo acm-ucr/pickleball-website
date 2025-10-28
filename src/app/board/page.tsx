6c9de8b2f8a84dacb8373dfb5a0e358e7bb615fe
sshen070 <SujithShen@gmail.com>
2025-10-28 13:50:45 -0700

    Made changes to add the first board member's image & description

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,21 @@
 import Header from "@/components/header";
+import BoardCard from "@/components/board/card";
+import bryan_image from "@/public/images/board/Bryan_Nguyen.webp";
 
 const Board = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center text-center">
       <Header title="Board" />
+      <div className="mt-8 flex flex-wrap justify-center gap-8">
+        <div className="flex flex-col items-center">
+        <BoardCard
+          image={bryan_image}
+          name="Bryan Nguyen"
+          position="President"
+        />
+      </div>
     </div>
+  </div> 
   );
 };
 
