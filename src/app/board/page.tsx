e36b1d02e41b6172040f5f2415a9ec77e60f36ea
sshen070 <SujithShen@gmail.com>
2025-10-31 11:29:42 -0700

    Made requested changes in card.tsx file

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,15 @@
-import Header from "@/components/header";
+import BoardCard from "@/components/board/card";
+import bryan_image from "@/public/images/board/Bryan_Nguyen.webp";
+
 
 const Board = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <Header title="Board" />
+      <BoardCard
+        image={bryan_image}
+        name="Bryan Nguyen"
+        position="President"
+      />
     </div>
   );
 };
