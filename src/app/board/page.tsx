473f07b4d50b845be405db63207a8948367f0009
sshen070 <SujithShen@gmail.com>
2025-10-31 11:40:37 -0700

    Few lines required to render image

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,15 +1,10 @@
 import BoardCard from "@/components/board/card";
 import bryan_image from "@/public/images/board/Bryan_Nguyen.webp";
 
-
 const Board = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <BoardCard
-        image={bryan_image}
-        name="Bryan Nguyen"
-        position="President"
-      />
+      <BoardCard image={bryan_image} name="Bryan Nguyen" position="President" />
     </div>
   );
 };
