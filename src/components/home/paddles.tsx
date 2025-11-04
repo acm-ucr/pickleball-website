91b340562b63ba813585f580c36f1bbaa53a0a4c
CruzL092 <cruzbusiness760@gmail.com>
2025-11-04 11:37:16 -0800

    Destructured paddles.tsx

--- src/components/home/paddles.tsx
+++ src/components/home/paddles.tsx
@@ -5,17 +5,13 @@ const Paddles = () => {
   return (
     <div className="mt-16 flex flex-wrap items-center justify-center">
       <div className="m-5 flex items-center justify-center">
-        {paddles.slice(0, 3).map((item, index) => (
-          <div key={index}>
-            <Paddle text={item.text} number={item.number} />
-          </div>
+        {paddles.slice(0, 3).map(({ text, number }, index) => (
+          <Paddle key={index} text={text} number={number} />
         ))}
       </div>
       <div className="m-5 -mt-60 mr-8 flex items-center justify-center">
-        {paddles.slice(3).map((item, index) => (
-          <div key={index + 3}>
-            <Paddle text={item.text} number={item.number} rotated={true} />
-          </div>
+        {paddles.slice(3).map(({ text, number }, index) => (
+          <Paddle key={index + 3} text={text} number={number} rotated={true} />
         ))}
       </div>
     </div>
