1119bfd54225360a3467bfbd964d8cbd1039175c
ManuChint <manu.chintalapathi@gmail.com>
2025-11-03 22:02:20 -0800

    added Join Us header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Landing from "@/components/home/landing";
+import Header from "@/components/header";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
       <Landing />
+
+      <div className="mt-12">
+        <Header title="Join Us" />
+      </div>
     </div>
   );
 };
