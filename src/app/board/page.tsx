71e764a45da6259924e80b0f5ec2e9e45f773de0
sshen070 <SujithShen@gmail.com>
2025-11-05 23:34:28 -0800

    Restored to original

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,10 +1,9 @@
-import BoardCard from "@/components/board/card";
-import bryan_image from "@/public/images/board/Bryan_Nguyen.webp";
+import Header from "@/components/header";
 
 const Board = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <BoardCard image={bryan_image} name="Bryan Nguyen" position="President" />
+      <Header title="Board" />
     </div>
   );
 };
