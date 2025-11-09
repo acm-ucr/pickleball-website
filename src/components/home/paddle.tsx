49dd5cfdea8136fef58472be420641615dfc24cc
CruzL092 <cruzbusiness760@gmail.com>
2025-11-09 12:56:30 -0800

    Mobile responsiveness changes

--- src/components/home/paddle.tsx
+++ src/components/home/paddle.tsx
@@ -11,7 +11,7 @@ interface PaddleProps {
 const Paddle = ({ text, number, className, rotated = false }: PaddleProps) => {
   return (
     <div
-      className={`relative m-12 flex items-center justify-center ${rotated ? "rotate-180" : ""} ${className}`}
+      className={`relative m-12 flex items-center justify-center ${rotated ? "lg:rotate-180" : ""} ${className}`}
     >
       <Image
         src={whitePaddle}
@@ -21,20 +21,20 @@ const Paddle = ({ text, number, className, rotated = false }: PaddleProps) => {
 
       {!rotated ? (
         <>
-          <div className="text-pickleball-green-100 absolute inset-x-16 inset-y-16 flex justify-center text-center text-4xl leading-relaxed font-bold tracking-wide">
+          <div className="text-pickleball-green-100 absolute inset-x-8 inset-y-12 flex justify-center text-center text-2xl leading-relaxed font-bold tracking-wide lg:inset-x-16 lg:inset-y-16 lg:text-4xl">
             {text}
           </div>
 
-          <div className="text-pickleball-green-100 absolute inset-x-2 inset-y-4 mt-24 flex items-center justify-center text-center text-6xl font-bold">
+          <div className="text-pickleball-green-100 absolute inset-x-2 inset-y-4 mt-24 flex items-center justify-center text-center text-4xl font-bold lg:text-6xl">
             {number}
           </div>
         </>
       ) : (
         <>
-          <div className="text-pickleball-green-100 absolute inset-x-10 inset-y-88 mr-6 mb-4 flex rotate-180 justify-center pr-4 text-center text-4xl leading-relaxed font-bold tracking-wide">
+          <div className="lg:text-pickleball-green-100 absolute inset-x-5 inset-y-12 ml-4 flex justify-center pr-4 text-center text-2xl leading-relaxed font-bold tracking-wide lg:inset-x-6 lg:inset-y-88 lg:mr-6 lg:mb-4 lg:ml-2 lg:rotate-180 lg:text-4xl">
             {text}
           </div>
-          <div className="text-pickleball-green-100 absolute inset-x-20 inset-y-2 mt-24 mr-4 flex rotate-180 items-center justify-center pr-4 text-center text-6xl font-bold">
+          <div className="lg:text-pickleball-green-100 absolute inset-x-15 inset-y-2 mt-24 ml-4 flex items-center justify-center pr-4 text-center text-4xl font-bold lg:mr-8 lg:rotate-180 lg:text-6xl">
             {number}
           </div>
         </>
--- src/components/home/paddles.tsx
+++ src/components/home/paddles.tsx
@@ -3,13 +3,13 @@ import { paddles } from "@/data/paddle";
 
 const Paddles = () => {
   return (
-    <div className="mt-16 flex flex-wrap items-center justify-center">
-      <div className="m-5 flex items-center justify-center">
+    <div className="lg:flex-flow mt-16 flex flex-col items-center justify-center lg:flex-wrap">
+      <div className="m-5 flex flex-col items-center justify-center lg:flex-row">
         {paddles.slice(0, 3).map(({ text, number }, index) => (
           <Paddle key={index} text={text} number={number} />
         ))}
       </div>
-      <div className="m-5 -mt-60 mr-8 flex items-center justify-center">
+      <div className="lmr-8 m-5 -mt-5 flex flex-col items-center justify-center lg:-mt-60 lg:flex-row">
         {paddles.slice(3).map(({ text, number }, index) => (
           <Paddle key={index + 3} text={text} number={number} rotated={true} />
         ))}
