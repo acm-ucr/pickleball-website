cf2490be28016d49b2455cb95d89a012adcf6edc
ManuChint <manu.chintalapathi@gmail.com>
2025-11-10 21:21:37 -0800

    removed space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <Landing />
-
       <Header title="JOIN US" />
     </div>
   );
