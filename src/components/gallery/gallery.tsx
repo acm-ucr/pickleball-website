6514b6684ec5e17359bafe1bcb1b5ed6411addd0
rishikpulipaka <rpuli007@ucr.edu>
2025-11-11 19:18:11 -0800

    destructured the map

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -6,11 +6,11 @@ const GalleryImages = () => {
     <div className="flex items-center justify-center py-10">
       <div className="w-full max-w-6xl px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {gallery.map((src, index) => (
+          {gallery.map(({ image, alt }, index) => (
             <div key={index} className="relative w-full overflow-hidden">
               <Image
-                src={src.image}
-                alt={src.alt}
+                src={image}
+                alt={alt}
                 className="h-auto w-full object-cover"
               />
             </div>
