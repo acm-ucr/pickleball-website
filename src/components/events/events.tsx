0bb5e8e0d15787712b15f99e23b54b4143871bbc
Mason Williams <mason190607@gmail.com>
2025-11-16 18:26:39 -0800

    Added animations to Weekly Events

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,7 +1,14 @@
+"use client";
 import Event from "@/components/events/event";
+import { motion } from "motion/react";
 const WeekEvents = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-15 pt-20 pb-20 md:flex-row md:gap-25">
+    <motion.div
+      className="flex w-full flex-col items-center justify-center gap-15 pt-20 pb-20 md:flex-row md:gap-25"
+      initial={{ opacity: 0, scale: 0 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ type: "spring", bounce: 0.3, duration: 0.8 }}
+    >
       <Event
         day="Monday"
         time="8pm"
@@ -15,7 +22,7 @@ const WeekEvents = () => {
         location="UCR SRC Pickleball Courts"
         description="Come out and play! Join us for a fun, social day of pickleball open to all players-beginners to advanced. Enjoy casual games, meet fellow players, and have a great time on the courts."
       />
-    </div>
+    </motion.div>
   );
 };
 export default WeekEvents;
