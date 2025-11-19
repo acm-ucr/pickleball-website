38d4ef75aadb2e64cfca13f552f7fc53f108358d
ntnkan089@gmail.com <ntnkan089@gmail.com>
2025-11-19 01:27:21 -0800

    made changes

--- src/components/home/bluepaddles.tsx
+++ src/components/home/bluepaddles.tsx
@@ -4,21 +4,19 @@ import { motion } from "motion/react";
 import Image from "next/image";
 import bluePaddle from "@/public/colored-paddle-ball.svg";
 
+const paddleMotion = {
+  y: [0, -20, 0],
+  x: [0, -3, 3, -3, 0],
+  rotate: [0, -5, 5, -5, 0],
+  transition: {
+    duration: 3,
+    repeat: Infinity,
+  },
+};
+
 const BluePaddle = () => {
   return (
-    <motion.div
-      initial={{ opacity: 1 }}
-      animate={{
-        opacity: 1,
-        y: [0, -20, 0],
-        x: [0, -3, 3, -3, 0],
-        rotate: [0, -5, 5, -5, 0],
-      }}
-      transition={{
-        duration: 1.2,
-        repeat: Infinity,
-      }}
-    >
+    <motion.div whileInView={paddleMotion}>
       <Image
         src={bluePaddle}
         alt="Blue pickleball paddle"
