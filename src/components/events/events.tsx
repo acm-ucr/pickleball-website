b9c94bde4692139a5dfbd13e2b03a8eff973019a
Mason Williams <mason190607@gmail.com>
2025-11-21 11:14:34 -0800

    move animation outside of component

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -1,27 +1,42 @@
 "use client";
 import Event from "@/components/events/event";
 import { motion } from "motion/react";
+
+const WeeklyAnimation = {
+  initial: { opacity: 0, scale: 0 },
+  whileInView: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      keyframes: [0, 1],
+      easing: "spring",
+      bounce: 0.3,
+    },
+  },
+};
+
 const WeekEvents = () => {
   return (
     <motion.div
-      className="flex w-full flex-col items-center justify-center gap-15 pt-20 pb-20 md:flex-row md:gap-25"
-      initial={{ opacity: 0, scale: 0 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{ type: "spring", bounce: 0.3, duration: 0.8 }}
+      variants={WeeklyAnimation}
+      initial="initial"
+      whileInView="whileInView"
     >
-      <Event
-        day="Monday"
-        time="8pm"
-        location="UCR SRC Pickleball Courts"
-        description="Come out and play! Join us for a fun, soical day of pickleball open to all players-beginners to advanced. Enjoy casual games, meet fellow players, and have a great time on the courts."
-      />
+      <div className="flex w-full flex-col items-center justify-center gap-15 pt-20 pb-20 md:flex-row md:gap-25">
+        <Event
+          day="Monday"
+          time="8pm"
+          location="UCR SRC Pickleball Courts"
+          description="Come out and play! Join us for a fun, soical day of pickleball open to all players-beginners to advanced. Enjoy casual games, meet fellow players, and have a great time on the courts."
+        />
 
-      <Event
-        day="Thursday"
-        time="6:30pm"
-        location="UCR SRC Pickleball Courts"
-        description="Come out and play! Join us for a fun, social day of pickleball open to all players-beginners to advanced. Enjoy casual games, meet fellow players, and have a great time on the courts."
-      />
+        <Event
+          day="Thursday"
+          time="6:30pm"
+          location="UCR SRC Pickleball Courts"
+          description="Come out and play! Join us for a fun, social day of pickleball open to all players-beginners to advanced. Enjoy casual games, meet fellow players, and have a great time on the courts."
+        />
+      </div>
     </motion.div>
   );
 };
