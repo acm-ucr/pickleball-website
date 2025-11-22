161715cd743f312e002385c21ded032a935151fb
ManuChint <manu.chintalapathi@gmail.com>
2025-11-21 16:12:42 -0800

    made ready to join responsive for smaller screens

--- src/components/home/readytojoin.tsx
+++ src/components/home/readytojoin.tsx
@@ -2,10 +2,10 @@ import Button from "@/components/home/button";
 
 const ReadyToJoin = () => {
   return (
-    <div className="bg-pickleball-green-200 text-pickleball-lime-100 flex w-full justify-center py-[12vh] px-4 sm:px-8">
-      <div className="flex flex-col items-center text-center gap-y-8 sm:gap-y-[5vh]">
-        <p className="text-4xl sm:text-5xl font-bold">READY TO JOIN?</p>
-        <p className="text-center text-base sm:text-lg max-w-xs sm:max-w-2xl">
+    <div className="bg-pickleball-green-200 text-pickleball-lime-100 flex w-full justify-center px-4 py-[12vh] sm:px-8">
+      <div className="flex flex-col items-center gap-y-8 text-center sm:gap-y-[5vh]">
+        <p className="text-4xl font-bold sm:text-5xl">READY TO JOIN?</p>
+        <p className="max-w-xs text-center text-base sm:max-w-2xl sm:text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor <br /> incididunt ut labore et dolore magna aliqua.
         </p>
