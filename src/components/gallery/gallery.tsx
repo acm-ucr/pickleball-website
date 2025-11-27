67142792b84a7637996199db594715716c1469e8
rishikpulipaka <rpuli007@ucr.edu>
2025-11-27 13:02:08 -0800

    formatting changes

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -7,10 +7,7 @@ const GalleryImages = () => {
       <div className="w-full max-w-6xl px-4">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3">
           {gallery.map(({ image, alt }, index) => (
-            <div
-              key={index}
-              className="relative w-full overflow-hidden"
-            >
+            <div key={index} className="relative w-full overflow-hidden">
               <Image
                 src={image}
                 alt={alt}
